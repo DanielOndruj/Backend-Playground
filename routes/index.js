a5fcd52178d311c034e7c32c0f4c5729c540e6fd
Daniel Ondruj <daniel.ondruj@email.cz>
2015-11-03 22:17:24 +0100

    Simple validate demo profile

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,31 @@ router.get('/hello', function(req, res, next) {
   res.send(data);
 });
 
+router.post('/profile', function (req, res, next) {
+	
+	req.checkBody('name', 'Name is empty').notEmpty()
+
+	req.checkBody('height', 'Height is empty').notEmpty()
+
+	req.checkBody('height', 'Height is invalid').isInt()
+
+	var valErrors = req.validateErrors()
+
+	if(valErrors) {
+		var data = {
+			error : 'Invalid Request',
+			message : valErrors
+		};
+
+		return res.send(data);
+	}
+
+	var profile = {
+		name: req.body.name,
+		height : req.body.height
+	};
+
+	res.send(profile);
+}
+
 module.exports = router;
