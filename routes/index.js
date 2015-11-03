a9d8c043b2c286495c1d00ca5c69b592ad678dc1
Daniel Ondruj <daniel.ondruj@email.cz>
2015-11-03 23:06:36 +0100

    Update npm dependencies

--- routes/index.js
+++ routes/index.js
@@ -2,40 +2,45 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/hello', function(req, res, next) {
-	var data = {
-		message: 'hello world'
-	};
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
+router.get('/hello', function(req, res, next) {
+
+	var data = {
+		message : 'hello world!'
+
+	};
 
   res.send(data);
 });
 
-router.post('/profile', function (req, res, next) {
-	
-	req.checkBody('name', 'Name is empty').notEmpty();
-
-	req.checkBody('height', 'Height is empty').notEmpty();
-
-	req.checkBody('height', 'Height is invalid').isInt();
-
-	var valErrors = req.validateErrors();
-
-	if(valErrors) {
-		var data = {
-			error : 'Invalid Request',
-			message : valErrors
-		};
-
-		return res.send(data);
-	}
-
-	var profile = {
-		name: req.body.name,
-		height : req.body.height
-	};
-
-	res.send(profile);
+router.post('/profile', function(req, res, next) {
+  
+  req.checkBody('name', 'Name is empty').notEmpty();
+  
+  req.checkBody('height', 'Height is empty').notEmpty();
+  
+  req.checkBody('height', 'Height is invalid').isInt();
+  
+  var valErrors = req.validationErrors();
+  
+  if (valErrors) {
+    var data = {
+      error: "InvalidRequest",
+      message: valErrors
+    };
+    return res.send(data);
+  }
+  
+  var profile = {
+    name: req.body.name,
+    height: req.body.height,
+    "asdf" : "asdf"
+  };
+  
+  res.send(profile);
 });
 
 module.exports = router;
